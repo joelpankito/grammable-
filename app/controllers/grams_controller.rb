db13b5276f3458b3480eaffe927ac7ef994506d8
joel pankito <joelpankito@yahoo.com>
2019-11-18 08:01:47 +0000

    lesson 19

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -22,7 +22,7 @@ class GramsController < ApplicationController
 	end
 
 	def index
-		@gram = Gram.find_by_id(params[:id])
+		 @grams = Gram.all
 	end
 
 	def new
@@ -52,10 +52,7 @@ class GramsController < ApplicationController
 	private
 
 	def gram_params
-		params.require(:gram).permit(:message)
+		params.require(:gram).permit(:message, :picture)
 	end
 
-	def render_not_found(status=:not_found)
-		render plain: "#{status.to_s.titleize} :(", status: status
-	end
 end
